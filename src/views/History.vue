f14df95146d913c1749e9112e399d41acbca9823
XCMG_houzilong <zhang_yu@xg.com>
2025-06-12 17:27:40 +0800

    feat: bug

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,9 +4,9 @@ import girl from '../assets/girl.jpg'
 import girls from '../assets/girls.jpg'
 import girl2 from '../assets/girl2.jpg'
 const urls = [
-  girl,
-  girl2,
-  girls,
+  // girl,
+  // girl2,
+  // girls,
 
   'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
   'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
